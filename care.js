f636116becf4a43530be83789c9ff4068df200e0
Utsho Majumder Joy <utsho.bma.acces@gmail.com>
2024-09-26 18:10:51 +0600

    R&D

---
+++ care.js
@@ -0,0 +1,46 @@
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#care-apps-home"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#care-apps-home" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#care-apps-home", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#care-apps-home").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+loco();
+
+tl1 = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#ai-section",
+    scroller:"#care-apps-home",
+    // markers:true,
+    pin:true,
+    start:"top top",
+    scrub:1
+  }
+})
+tl1.to("#ai-section>h3",{
+  top:"-50%"
+})
