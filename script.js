3b7456fc91b1130a3536e02251ca3e510627e646
Utsho Majumder Joy <utsho.bma.acces@gmail.com>
2024-10-18 22:08:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -553,12 +553,3 @@ gsap.to("#page25>img",{
   },
   opacity:1
 })
-
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
